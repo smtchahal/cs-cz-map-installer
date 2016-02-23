2ae1b6e9472f60f58cc69549aba7f3dc0bbb5516
Sumit Chahal <smtchahal@gmail.com>
2016-02-23 18:54:40 +0530

    Version bump: v0.1.1

--- cs_cz_map_installer/app.py
+++ cs_cz_map_installer/app.py
@@ -5,7 +5,7 @@ from PySide import QtGui
 from .mainwindow import MainWindow
 
 QtGui.QApplication.setApplicationName('CS:CZ Map Installer')
-QtGui.QApplication.setApplicationVersion('0.1')
+QtGui.QApplication.setApplicationVersion('0.1.1')
 
 def run():
     app = QtGui.QApplication(sys.argv)
